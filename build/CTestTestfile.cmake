489ce3c0160cdfa6709d5e0d025d4088fd7e3442
Tariq Zahroof <tzahroof@stanford.edu>
2018-04-07 00:48:39 -0700

    Robot Arm and Launch File Created for ROS Environment

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/tariq/catkin_ws/src/restorebot_desc
+# Build directory: /home/tariq/catkin_ws/src/restorebot_desc/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/tariq/catkin_ws/src/restorebot_desc/build/devel:$CMAKE_PREFIX_PATH"
+export ROS_PACKAGE_PATH="/home/tariq/catkin_ws/src/restorebot_desc:$ROS_PACKAGE_PATH"
